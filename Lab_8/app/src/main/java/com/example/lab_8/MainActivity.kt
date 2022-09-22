1284fd0a9657042df05f89c922a291e3aefa6b88
Samuel Argueta <arg211024@uvg.edu.gt>
2022-09-22 09:38:22 -0600

    Inicio de la logica

--- Lab_8/app/src/main/java/com/example/lab_8/MainActivity.kt
+++ Lab_8/app/src/main/java/com/example/lab_8/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.lab_8
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -12,6 +13,7 @@ import com.google.android.material.appbar.MaterialToolbar
 class MainActivity : AppCompatActivity() {
     private lateinit var toolBarName: MaterialToolbar
     private lateinit var navController: NavController
+    private lateinit var startSessionbutton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +31,8 @@ class MainActivity : AppCompatActivity() {
         toolBarName = findViewById(R.id.toolbar_mainActivity)
         toolBarName.setupWithNavController(navController, appBarConfig)
         listenToNavGraphChanges()
+
+        startSessionbutton = findViewById(R.id.btn_fragmentLogIn_startSession)
     }
 /*
     private fun setListeners(){
@@ -47,9 +51,15 @@ class MainActivity : AppCompatActivity() {
         }
     }
 */
+    /**
+     * Showing the tool bar to each screen
+     */
     private fun listenToNavGraphChanges(){
         navController.addOnDestinationChangedListener(NavController.OnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
+                R.id.logInFragment ->{
+                    toolBarName.visibility = View.INVISIBLE
+                }
                 R.id.detailsCharacterFragment -> {
                     toolBarName.menu.findItem(R.id.menu_itemAscendentSort).isVisible = false
                     toolBarName.menu.findItem(R.id. menu_itemDescendentSort).isVisible = false
